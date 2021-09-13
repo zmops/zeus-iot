b82b352063960c6357a66a285b116a5f992a35ad
nantian <nantian@zmops.com>
2021-09-13 12:00:06 +0800

    [style]: change code style

--- zeus-webapp/src/main/java/com/zmops/iot/web/product/controller/ProductEventTriggerController.java
+++ zeus-webapp/src/main/java/com/zmops/iot/web/product/controller/ProductEventTriggerController.java
@@ -1,16 +1,24 @@
 package com.zmops.iot.web.product.controller;
 
+import com.zmops.iot.model.response.ResponseData;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author nantian created at 2021/8/7 11:58
  * <p>
- * 配置告警规则
+ * 配置告警规则  属性 事件  触发 方法回调
  */
 @RestController
-@RequestMapping("/product/trigger/event")
+@RequestMapping("/product/event/trigger")
 public class ProductEventTriggerController {
 
-        
+
+    @RequestMapping("/create")
+    public ResponseData createEventTrigger() {
+        return ResponseData.success();
+    }
+
+
+
 }
