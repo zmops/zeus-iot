500deb453a784611df934457cee85bd1670e08e2
nantian <nantian@zmops.com>
2021-09-15 11:40:15 +0800

    [style]: change code style

--- zeus-webapp/src/main/java/com/zmops/iot/web/device/schedule/DeviceOnlineReprotSchedule.java
+++ zeus-webapp/src/main/java/com/zmops/iot/web/device/schedule/DeviceOnlineReprotSchedule.java
@@ -30,12 +30,18 @@ public class DeviceOnlineReprotSchedule {
         log.info("开始查询设备在线情况");
 
         //统计出 当前 设备在线情况
-        List<Device>                    deviceList = new QDevice().findList();
-        Map<String, Map<Integer, Long>> onLinemap = deviceList.parallelStream().collect(Collectors.groupingBy(Device::getType, Collectors.groupingBy(Device::getOnline, Collectors.counting())));
-        List<DeviceOnlineReport>        list      = new ArrayList<>();
+        List<Device> deviceList = new QDevice().findList();
+
+        Map<String, Map<Integer, Long>> onLinemap = deviceList.parallelStream()
+                .collect(Collectors.groupingBy(Device::getType, Collectors.groupingBy(Device::getOnline, Collectors.counting())));
+
+        List<DeviceOnlineReport> list = new ArrayList<>();
+
         onLinemap.forEach((key, value) -> {
-            list.add(DeviceOnlineReport.builder().type(Integer.parseInt(key)).createTime(LocalDateTimeUtils.formatTimeDate(LocalDateTime.now()))
-                    .online(Optional.ofNullable(value.get(1)).orElse(0L)).offline(Optional.ofNullable(value.get(0)).orElse(0L)).build());
+            list.add(DeviceOnlineReport.builder().type(Integer.parseInt(key))
+                    .createTime(LocalDateTimeUtils.formatTimeDate(LocalDateTime.now()))
+                    .online(Optional.ofNullable(value.get(1)).orElse(0L))
+                    .offline(Optional.ofNullable(value.get(0)).orElse(0L)).build());
         });
         //插入 在线情况表
         DB.saveAll(list);
