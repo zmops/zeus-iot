cf71adcb43a895f99332b070b104f760adaffcc8
yefei <yefei@zmops.com>
2021-09-18 19:01:35 +0800

    [fix]: six sh script

--- dist-material/bin/stop.sh
+++ dist-material/bin/stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 status=`ps -ef | grep zeus-iot-bin | grep -v grep | awk '{print $2}' | wc -l`
 
-if [[ $status -ne 0 ]]
+if [ $status -ne 0 ]
 then
 	for i in `ps -ef | grep zeus-iot-bin | grep -v grep | awk '{print $2}'`
 	do
