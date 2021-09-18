8c2937a64bc72539ef3615cea1e0af22197ccc89
yefei <yefei@zmops.com>
2021-09-18 17:15:06 +0800

    [fix]: fix sh script

--- dist-material/bin/startup.sh
+++ dist-material/bin/startup.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 source /etc/profile
 PRG="$0"
 PRGDIR=`dirname "$PRG"`
