0893ce77bd6d8aeca5faf7d515b5677ebaaf5313
chunyu@zmops.com <chunyu@zmops.com>
2021-09-20 17:01:43 +0800

    [fix]: fix stop.sh

--- dist-material/bin/stop.sh
+++ dist-material/bin/stop.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -x
-ps -ef | grep zeus-iot-bin | grep -v grep > /tmp/test
+
 status=`ps -ef | grep zeus-iot-bin | grep java | grep -v grep | awk '{print $2}' | wc -l`
 
 if [ $status -ne 0 ]
 then
-	for i in `ps -ef | grep zeus-iot-bin | grep -v grep | awk '{print $2}'`
+	for i in `ps -ef | grep zeus-iot-bin | grep java | grep -v grep | awk '{print $2}'`
 	do
 		kill -9 $i
 	done
