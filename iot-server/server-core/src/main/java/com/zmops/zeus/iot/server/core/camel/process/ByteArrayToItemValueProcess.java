df22ff44305e59fb17a03bd53ffd384d6b83e99f
yefei <yefei@zmops.com>
2021-09-23 13:51:19 +0800

    [fix]: mqtt reciver update

--- iot-server/server-core/src/main/java/com/zmops/zeus/iot/server/core/camel/process/ByteArrayToItemValueProcess.java
+++ iot-server/server-core/src/main/java/com/zmops/zeus/iot/server/core/camel/process/ByteArrayToItemValueProcess.java
@@ -1,15 +1,16 @@
 package com.zmops.zeus.iot.server.core.camel.process;
 
 import com.google.gson.Gson;
-import com.zmops.zeus.iot.server.core.camel.IOTDeviceValue;
 import com.zmops.zeus.iot.server.core.worker.data.ItemValue;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
+import org.apache.commons.lang3.StringUtils;
 
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author nantian created at 2021/8/23 17:16
@@ -27,17 +28,21 @@ public class ByteArrayToItemValueProcess implements Processor {
 
         String inputContext = new String(messageBytes, StandardCharsets.UTF_8);
 
-        IOTDeviceValue iotValue = gson.fromJson(inputContext, IOTDeviceValue.class);
-
+        Map<String, String> jsonMap = gson.fromJson(inputContext, Map.class);
+        String              key     = jsonMap.get("key");
+        String              value   = jsonMap.get("value");
+        if (StringUtils.isBlank(key)) {
+            return;
+        }
+        String          deviceId      = key.substring(0, key.indexOf("."));
+        String          attributeKey  = key.substring(key.indexOf(".") + 1);
         List<ItemValue> itemValueList = new ArrayList<>();
 
-        iotValue.getAttributes().forEach((key, value) -> {
-            ItemValue item = new ItemValue(iotValue.getDeviceId(), iotValue.getClock());
-            item.setKey(key);
-            item.setValue(value);
+        ItemValue item = new ItemValue(deviceId, System.currentTimeMillis()/1000);
+        item.setKey(attributeKey);
+        item.setValue(value);
 
-            itemValueList.add(item);
-        });
+        itemValueList.add(item);
 
         exchange.getMessage().setBody(itemValueList);
     }
--- iot-server/server-receiver-plugin/mqtt-receiver-plugin/src/main/java/com/zmops/zeus/iot/server/receiver/mqtt/provider/MqttRouteBuilder.java
+++ iot-server/server-receiver-plugin/mqtt-receiver-plugin/src/main/java/com/zmops/zeus/iot/server/receiver/mqtt/provider/MqttRouteBuilder.java
@@ -17,7 +17,7 @@ public class MqttRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        fromF("mqtt:zeus-iot-mqtt?host=tcp://172.16.3.72:%d&subscribeTopicNames=test.mqtt.topic", mqttReceiverConfig.getPort())
+        fromF("mqtt:zeus-iot-mqtt?host=tcp://%s:%d&subscribeTopicNames=34EAE784BC4A/up", mqttReceiverConfig.getHost(),mqttReceiverConfig.getPort())
                 .process(new ByteArrayToItemValueProcess())
                 .to("Zabbix");
     }
