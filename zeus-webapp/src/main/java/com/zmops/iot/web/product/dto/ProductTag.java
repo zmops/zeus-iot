221e82190c95a70d480d9c30a8d463faf82d53e0
yefei <yefei@zmops.com>
2021-10-02 21:42:36 +0800

    [fix]: fix tag regexp pattern

--- zeus-webapp/src/main/java/com/zmops/iot/web/product/dto/ProductTag.java
+++ zeus-webapp/src/main/java/com/zmops/iot/web/product/dto/ProductTag.java
@@ -2,8 +2,11 @@ package com.zmops.iot.web.product.dto;
 
 import lombok.Data;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 /**
@@ -18,15 +21,18 @@ public class ProductTag {
     @NotNull
     private String productId;
 
+    @Valid
     private List<Tag> productTag;
 
     @Data
     public static class Tag {
 
-        @Max(20)
+        @Size(max = 20)
+        @Pattern(regexp = "^[A-Za-z0-9]+$",
+                message = "标签名称不能包含中文、特殊字符")
         private String tag;
 
-        @Max(50)
+        @Size(max = 50)
         private String value;
     }
 }
