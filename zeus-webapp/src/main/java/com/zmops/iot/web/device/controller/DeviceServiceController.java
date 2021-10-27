47bee11619c9462f01383f4050e41fbd2308d0a4
yefei <yefei@zmops.com>
2021-10-27 15:32:57 +0800

    [fix]: add service execute

---
+++ zeus-webapp/src/main/java/com/zmops/iot/web/device/controller/DeviceServiceController.java
@@ -0,0 +1,26 @@
+package com.zmops.iot.web.device.controller;
+
+import com.zmops.iot.model.response.ResponseData;
+import com.zmops.iot.web.device.service.DeviceSvrService;
+import org.apache.camel.spi.AsEndpointUri;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yefei
+ **/
+@RestController
+@RequestMapping("/device/service")
+public class DeviceServiceController {
+
+    @Autowired
+    DeviceSvrService deviceSvrService;
+
+    @RequestMapping("/execute")
+    public ResponseData execute(@RequestParam("deviceId") String deviceId,@RequestParam("serviceId") Long serviceId){
+        deviceSvrService.execute(deviceId,serviceId);
+        return ResponseData.success();
+    }
+}
---
+++ zeus-webapp/src/main/java/com/zmops/iot/web/device/service/DeviceSvrService.java
@@ -0,0 +1,58 @@
+package com.zmops.iot.web.device.service;
+
+import com.alibaba.fastjson.JSON;
+import com.dtflys.forest.Forest;
+import com.zmops.iot.domain.device.Device;
+import com.zmops.iot.domain.device.query.QDevice;
+import com.zmops.iot.domain.product.ProductService;
+import com.zmops.iot.domain.product.ProductServiceParam;
+import com.zmops.iot.domain.product.query.QProductService;
+import com.zmops.iot.domain.product.query.QProductServiceParam;
+import com.zmops.iot.model.exception.ServiceException;
+import com.zmops.iot.util.ToolUtil;
+import com.zmops.iot.web.exception.enums.BizExceptionEnum;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+/**
+ * @author yefei
+ **/
+@Service
+public class DeviceSvrService {
+
+    public void execute(String deviceId, Long serviceId) {
+
+        List<Map<String, Object>> list = new ArrayList<>();
+
+        Map<String, Object> map = new ConcurrentHashMap<>(2);
+        Device device = new QDevice().deviceId.eq(deviceId).findOne();
+        if (null == device) {
+            throw new ServiceException(BizExceptionEnum.DEVICE_NOT_EXISTS);
+        }
+        map.put("device", device);
+
+        List<Map<String, Object>> serviceList = new ArrayList<>();
+        Map<String, Object> serviceMap = new ConcurrentHashMap<>();
+        ProductService productService = new QProductService().id.eq(serviceId).findOne();
+        if (null == productService) {
+            throw new ServiceException(BizExceptionEnum.SERVICE_NOT_EXISTS);
+        }
+        serviceMap.put("name", productService.getName());
+
+        List<ProductServiceParam> paramList = new QProductServiceParam().serviceId.eq(serviceId).findList();
+        if (ToolUtil.isNotEmpty(paramList)) {
+            serviceMap.put("param", paramList.parallelStream().collect(Collectors.toMap(ProductServiceParam::getKey, ProductServiceParam::getValue)));
+        }
+        serviceList.add(serviceMap);
+
+        map.put("service", serviceList);
+        list.add(map);
+
+        Forest.post("/device/action/exec").host("127.0.0.1").port(12800).contentTypeJson().addBody(JSON.toJSON(list)).execute();
+    }
+}
