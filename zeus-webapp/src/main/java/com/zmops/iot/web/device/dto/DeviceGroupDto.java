32a4ed4330714e142a80695f248d17430a5b35ab
yefei <yefei@zmops.com>
2021-11-02 14:03:47 +0800

    [fix]: fix devicegroup list

--- zeus-webapp/src/main/java/com/zmops/iot/web/device/dto/DeviceGroupDto.java
+++ zeus-webapp/src/main/java/com/zmops/iot/web/device/dto/DeviceGroupDto.java
@@ -1,29 +1,12 @@
-/**
- * Copyright 2018-2020 stylefeng & fengshuonan (https://gitee.com/stylefeng)
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.zmops.iot.web.device.dto;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.zmops.iot.domain.BaseEntity;
 import com.zmops.iot.model.cache.filter.CachedValue;
 import com.zmops.iot.model.cache.filter.CachedValueFilter;
 import com.zmops.iot.model.cache.filter.DicType;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 
 /**
  * 设备组传输bean
@@ -41,9 +24,9 @@ public class DeviceGroupDto {
     private String remark;
 
     @CachedValue(type = DicType.SysUserName)
-    private Long createUser;
-    private String createTime;
+    private Long          createUser;
     @CachedValue(type = DicType.SysUserName)
-    private Long updateUser;
-    private String updateTime;
+    private Long          updateUser;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
 }
