75a1b78a8e8c0f56975cc68877b47f55c6ae5dd0
nantian <nantian@zmops.com>
2021-12-15 14:13:49 +0800

    [feat]: remove gateway web console

--- iot-server/server-starter/src/main/webapp/assets/zeus/json/upload/demo.js
+++
@@ -1,7 +0,0 @@
-{
-  "code": 0,
-  "msg": "",
-  "data": {
-    "src": "https://sentsin.gitee.io/res/images/layui/avatar.png"
-  }
-}
--- iot-server/server-starter/src/main/webapp/assets/zeus/json/user/session.js
+++
@@ -1,9 +0,0 @@
-{
-  "code": 0
-  ,"msg": ""
-  ,"data": {
-    "username": "tester"
-    ,"sex": "男"
-    ,"role": 1
-  }
-}
